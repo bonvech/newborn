85682474457490d3cdbd6146005cc7827b0b3321
bonvech <bonvech@yandex.ru>
2024-04-25 01:00:46 +0300

    TCA data copy

--- Manager/copier.py
+++ Manager/copier.py
@@ -8,12 +8,12 @@ def safecopy(source_file, target_dir):
     ## check path to copy to
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir)
-    res = os.system(f"copy {source_file}  {target_dir}")
+    res = os.system(f"xcopy {source_file}  {target_dir} /d /y")
     if res:
         print("\r\r", source_file, target_dir)
     
 
-
+  
 ###  config directories
 source_dir = "d:\\AK\\"
 destin_dir = "d:\\DATA\\"
@@ -52,10 +52,17 @@ for ftype in ["2_day.png", "2_day.svg", "2_week.png", "all_day.png", "all_day.sv
     safecopy(f"{source_dir}Web_MEM_Suxarevskaya\\figures\\web_suxarevskaya_{ftype}",  f"{figure_dir}Web_MEM_Suxarevskaya")
 
 ##  LVS, PNS
-## D:\AK\PNS\src\lvs\figures
+##  D:\AK\PNS\src\lvs\figures
 for ftype in [".png", ".svg"]:
     safecopy(f"{source_dir}LVS\\src\\lvs\\figures\\lvs_week{ftype}",  f"{figure_dir}LVS")
     safecopy(f"{source_dir}PNS\\src\\lvs\\figures\\pns_week{ftype}",  f"{figure_dir}PNS")
+
+##  TCA08
+##  D:\AK\TCA08\figures
+for ftype in [".png", ".svg"]:
+    for period in ["day", "week"]:
+        safecopy(f"{source_dir}TCA08\\figures\\tca_extdevicedata_{period}{ftype}",  f"{figure_dir}TCA08")
+        safecopy(f"{source_dir}TCA08\\figures\\tca_onlineresult_{period}{ftype}",  f"{figure_dir}TCA08")
 
 
 
@@ -88,16 +95,17 @@ for timestamp in timestamps:
 
     ## TCA
     timestamp = "-".join(timestamp.split("_"))
-    safecopy(f"{source_dir}TCA\\data\\Data\\{timestamp}_Data.csv",    f"{destin_dir}TCA08\\Data")
-    safecopy(f"{source_dir}TCA\\data\\Logs\\{timestamp}_Logs.csv",    f"{destin_dir}TCA08\\Logs")
-    safecopy(f"{source_dir}TCA\\data\\Setup\\{timestamp}_Setup.csv",  f"{destin_dir}TCA08\\Setup")
-    safecopy(f"{source_dir}TCA\\data\\OffLineData\\{timestamp}_OffLineData.csv",     f"{destin_dir}TCA08\\OffLineData")
-    safecopy(f"{source_dir}TCA\\data\\OnLineResult\\{timestamp}_OnLineResult.csv",   f"{destin_dir}TCA08\\OnLineResult")
-    safecopy(f"{source_dir}TCA\\data\\OnLineResult\\{timestamp}_OnLineResult.xlsx",  f"{destin_dir}TCA08\\OnLineResult")
-    safecopy(f"{source_dir}TCA\\data\\OnLineResult\\{timestamp}_OnLineResult.csv",   f"{destin_dir}TCA08")
-    safecopy(f"{source_dir}TCA\\data\\OnLineResult\\{timestamp}_OnLineResult.xlsx",  f"{destin_dir}TCA08")
-    safecopy(f"{source_dir}TCA\\data\\ExtDeviceData\\{timestamp}_ExtDeviceData.csv", f"{destin_dir}TCA08\\ExtDeviceData")
-    safecopy(f"{source_dir}TCA\\data\\ExtDeviceData\\{timestamp}_ExtDeviceData.txt", f"{destin_dir}TCA08\\ExtDeviceData")
+    tca_dir = f"{source_dir}TCA08"
+    safecopy(f"{tca_dir}\\data\\Data\\{timestamp}_Data.csv",    f"{destin_dir}TCA08\\Data")
+    safecopy(f"{tca_dir}\\data\\Logs\\{timestamp}_Logs.csv",    f"{destin_dir}TCA08\\Logs")
+    safecopy(f"{tca_dir}\\data\\Setup\\{timestamp}_Setup.csv",  f"{destin_dir}TCA08\\Setup")
+    safecopy(f"{tca_dir}\\data\\OffLineData\\{timestamp}_OffLineData.csv",     f"{destin_dir}TCA08\\OffLineData")
+    safecopy(f"{tca_dir}\\data\\OnLineResult\\{timestamp}_OnLineResult.csv",   f"{destin_dir}TCA08\\OnLineResult")
+    safecopy(f"{tca_dir}\\data\\OnLineResult\\{timestamp}_OnLineResult.xlsx",  f"{destin_dir}TCA08\\OnLineResult")
+    safecopy(f"{tca_dir}\\data\\OnLineResult\\{timestamp}_OnLineResult.csv",   f"{destin_dir}TCA08")
+    safecopy(f"{tca_dir}\\data\\OnLineResult\\{timestamp}_OnLineResult.xlsx",  f"{destin_dir}TCA08")
+    safecopy(f"{tca_dir}\\data\\ExtDeviceData\\{timestamp}_ExtDeviceData.csv", f"{destin_dir}TCA08\\ExtDeviceData")
+    safecopy(f"{tca_dir}\\data\\ExtDeviceData\\{timestamp}_ExtDeviceData.txt", f"{destin_dir}TCA08\\ExtDeviceData")
 
 
 
@@ -108,7 +116,26 @@ print("\n====  copy DATA to Yandex.Disk")
 AKdata_dir = "D:\\DATA"
 yandex_dir = '"D:\\AerosolComplex\\YandexDisk\\ИКМО org.msu\\DATA\\"'
 
-#command = f"xcopy {AKdata_dir} {yandex_dir} /e /y"
-command = f"xcopy {AKdata_dir} {yandex_dir} /e /y"
+##  Обновить старые файлы и добавить новые
+##  /C - Продолжение копирования вне зависимости от наличия ошибок.
+##  /E - Копирование каталогов с подкаталогами, включая пустые. Эквивалентен сочетанию ключей /S /E. Совместим с ключом /T.
+##  /Y - Подавление запроса подтверждения на перезапись существующего целевого файла.
+##  /D:m-d-y - Копирование файлов, измененных не ранее указанной даты. Если дата не указана, заменяются только конечные файлы, более старые, чем исходные.
+command = f"xcopy {AKdata_dir}\\* {yandex_dir} /E /Y /D"
 print(command)
 os.system(command)
+
+##  Обновить старые файлы
+##  /R - Разрешение замены файлов, предназначенных только для чтения (среди прочих).
+##  /S - Замена файлов во всех подкаталогах конечного каталога. Этот ключ несовместим с ключом /A.
+##  /U - Замена только файлов, более старых, чем исходные. Этот ключ несовместим с ключом /A.
+#command = f"replace {AKdata_dir}\\* {yandex_dir} /R /S /U "
+#print(command)
+#os.system(command)
+
+##  Добавление новых файлов
+##  REPLACE
+##  /A - Добавление новых файлов в конечный каталог. Этот ключ несовместим с ключами /S и /U.
+#command = f"replace {AKdata_dir}\\* {yandex_dir} /A"
+#print(command)
+#os.system(command)
